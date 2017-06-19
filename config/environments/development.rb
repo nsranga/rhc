ff424223d657f4dd62d6fe8ea8e2d2fdc2793e69
Ranganadhan <nsranga@ranganadhan-Linux16.04>
2017-06-19 16:02:44 +0530

    serving static assets in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # settings for local precompilation 
+  # config.assets.prefix = "/dev-assets"
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
